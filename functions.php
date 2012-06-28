d63a876590ec0fbb3a7994f53cb1efd9bbc60ae3
Andrew Beeken <abeeken@gmail.com>
2012-06-28 20:05:53 +0100

    First commit with theme contents

---
+++ functions.php
@@ -0,0 +1,34 @@
+<?php
+	$args = array(
+	'name'          => 'Homepage Top',
+	'id'            => 'homepage-top',
+	'description'   => 'Content for the top block of the homepage',
+	'before_widget' => '<div id="%1$s" class="widget bg_light padding10 %2$s">',
+	'after_widget'  => '</div>',
+	'before_title'  => '<h3 class="widgettitle">',
+	'after_title'   => '</h3>' );
+	
+	register_sidebar( $args );
+	
+	$args = array(
+	'name'          => 'Homepage Left',
+	'id'            => 'homepage-left',
+	'description'   => 'Content for the left block of the homepage',
+	'before_widget' => '<div id="%1$s" class="widget bg_light padding10 %2$s">',
+	'after_widget'  => '</div>',
+	'before_title'  => '<h3 class="widgettitle">',
+	'after_title'   => '</h3>' );
+	
+	register_sidebar( $args );
+	
+	$args = array(
+	'name'          => 'Homepage Right',
+	'id'            => 'homepage-right',
+	'description'   => 'Content for the right block of the homepage',
+	'before_widget' => '<div id="%1$s" class="widget bg_light padding10 %2$s">',
+	'after_widget'  => '</div>',
+	'before_title'  => '<h3 class="widgettitle">',
+	'after_title'   => '</h3>' );
+	
+	register_sidebar( $args );
+?>
---
+++ tpl-homepage.php
@@ -0,0 +1,75 @@
+<?php
+/*
+Template Name: Homepage
+*/
+?>
+<?php get_header() ?>
+
+<section id="cwd_content" class="cwd_container" data-template="page">
+
+	<div class="grid_12 column bg_white">
+
+		<div class="grid_two_thirds column first">
+										
+				<?php if(have_posts()): ?>
+				
+					<?php while(have_posts()): the_post(); ?>
+				
+						<article class="article">
+				
+							<!--<h1 class="post-title padding_top padding_left">
+								<a href="<?php the_permalink(); ?>">
+									<?php the_title(); ?>
+								</a>
+							</h1>-->
+														
+							
+							<?php if(function_exists('wp_content_slider')) : ?>
+							<div class="padding_left padding_top">
+								<?php wp_content_slider(); ?>
+							</div>
+							<?php endif; ?>
+							
+							<?php if( is_active_sidebar ( 'homepage-top' ) ) : ?>
+							<div class="padding_left padding_top">
+								<?php dynamic_sidebar( 'homepage-top' ); ?>
+							</div>
+							<?php endif ?>
+							
+							<div class="post-content padding_left padding_top library-home column first">
+								<?php dynamic_sidebar( 'homepage-left' ); ?>
+							</div>
+							
+							<div class="post-content padding_top library-home column last">
+								<?php dynamic_sidebar( 'homepage-right' ); ?>
+							</div>
+							
+							<?php if(is_buddypress()): ?><hr class="margin_left"><?php endif; ?>
+									
+						</article>
+					
+					<?php endwhile; ?>
+				
+				<?php endif; ?>
+				
+			<?php if(is_buddypress()): ?>
+			<aside class="grid_4 column first">
+				<?php if ( ! dynamic_sidebar( 'frontpage-bottom-left' ) ) : ?><?php endif; ?>
+				&nbsp;
+			</aside>	
+			
+			<aside class="grid_4 column last">
+				<?php if ( ! dynamic_sidebar( 'frontpage-bottom-right' ) ) : ?><?php endif; ?>
+				&nbsp;
+			</aside>								
+			<?php endif; ?>
+
+		</div>
+		
+		<?php get_sidebar() ?>
+	
+	</div>
+
+</section>
+
+<?php get_footer() ?>
